c0f4e7b7f0935ab46d30bce74c37c7fe19616420
jose miguel Hernandez <aguila_miguel83@hotmail.com>
2024-02-27 17:08:08 -0600

    Cuarto commit tablaDinamica

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Programmer(models.models):
+    name=models.CharField(max_length=50)
+    country=models.CharField(max_length=3)
+    birthday = models.DateField()
+    score = models.PositiveSmallIntegerField()
+
+    class Meta:
+        db_table='programmer'
