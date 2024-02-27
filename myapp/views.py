a04a23aac197e04cc8dfd8accb6a469a1551ae1f
jose miguel Hernandez <aguila_miguel83@hotmail.com>
2024-02-27 15:49:30 -0600

    Segundo commit

--- myapp/views.py
+++ myapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def hello (request):
 
-    return HttpResponse("<h2>Holaaaaaa</h2>")
+    return HttpResponse("<h2>Reclasificadoor</h2>" )
 
 def prueba (request):
     return HttpResponse("Prueba")
