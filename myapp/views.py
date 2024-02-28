da0c48bb802140d4f3d616d5acd653cc1ca7eb3e
jose miguel Hernandez <aguila_miguel83@hotmail.com>
2024-02-28 16:45:12 -0600

    plantillas Bootstrap

--- myapp/views.py
+++ myapp/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
+from django.template import Template, Context
 
 # Create your views here.
 def hello (request):
+    doc_externo = open("C:/Users/SistemasGSV/Desktop/djangoproject/myapp/Plantillas/misplantillas.html")
+    plt = Template(doc_externo.read()) #Se deben importar Template y context
 
-    return HttpResponse("<h2>Reclasificadoor</h2>" )
+    doc_externo.close
+
+    cxt = Context()
+
+    documento = plt.render(cxt)
+
+    return HttpResponse(documento)
 
 def prueba (request):
-    return HttpResponse("Prueba")
+    doc_externo_prueba = open("C:/Users/SistemasGSV/Desktop/djangoproject/myapp/Plantillas/index.html")
+    plt_prueba = Template(doc_externo_prueba.read()) #Se deben importar Template y context
+
+    doc_externo_prueba.close
+
+    cxt_prueba = Context()
+
+    documento_prueba = plt_prueba.render(cxt_prueba)
+
+    return HttpResponse(documento_prueba)
 
