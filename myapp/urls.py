05401612a1810d37dcee0b8c2362f6f0ad4e69f3
jose miguel Hernandez <aguila_miguel83@hotmail.com>
2024-03-11 17:23:07 -0600

    Tablas y botones edit-delete-update

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.get_proveedores_list),
+    path('addProv/', views.addProv),
+    path('updateprov/<int:id_proveedor>', views.updateprov),
+    path('deleteprov/<int:id_proveedor>', views.deleteprov),
+
+    
     path('prueba/', views.prueba),
-    path('tabla/', views.get_proveedores_list)
+    path('tabla/', views.get_proveedores_list),
+    
+    
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,99 +1,16 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.http import Http404
+from django.shortcuts import render, redirect
+#from django.http import HttpResponse
+#from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 #from django.urls import reverse
-from django.contrib import messages
-from django.contrib.messages.views import SuccessMessageMixin
-from .models import clasf_Proveedor_Tabla
+#from django.contrib import messages
+#from django.contrib.messages.views import SuccessMessageMixin
+#from .models import clasf_Proveedor_Tabla
+from .forms import ProvForm
+from django.apps import apps
+from django.core.paginator import Paginator
 import pyodbc
-'''
-class ClasifListado(ListView):
-    model = clasf_Proveedor_Tabla
-
-class ClasifCrear(SuccessMessageMixin, CreateView):
-    model = clasf_Proveedor_Tabla
-    fields = "__all__"
-    success_message = "Clasificación creada correctamente"
-
-    def get_success_url(self):
-        return reverse('leer')
-
-class ClasifDetalle(DetailView):
-    model = clasf_Proveedor_Tabla
-
-class ClasifActualizar(SuccessMessageMixin, UpdateView):
-    model = clasf_Proveedor_Tabla
-    fields = "__all__"
-    success_message = "Registro actualizado correctamente"
-
-    def get_success_url(self):
-        return reverse('leer')
-
-class ClasifEliminar(SuccessMessageMixin, DeleteView):
-    model = clasf_Proveedor_Tabla
-
-    def get_success_url(self):
-        success_message = 'Clasificación eliminada correctamente'
-        messages.success(self.request, success_message)
-        return reverse('leer')
-
-# Vista para la conexión a la base de datos
-def hello(request):
-    try:
-        with pyodbc.connect('Driver={sql server};'
-                            'Server=gsvwdb17\sql2014;'
-                            'Database=Pruebas3;'
-                            'UID=gsvreportes;'
-                            'PWD=Ind2019&;'
-                            'Trusted_Connection=no;') as conn:
-            cursor = conn.cursor()
-            query = "SELECT * FROM zClasifProveedores"
-            cursor.execute(query)
-            result = cursor.fetchall()
-            # Puedes hacer algo con el resultado aquí, como pasarlo al contexto y renderizar en una plantilla
-            return render(request, 'index.html', {'result': result})
-    except pyodbc.Error as e:
-        # Manejar el error según tus necesidades
-        return HttpResponse(f"Error de base de datos: {e}")
-
-    #return render(request, 'misplantillas.html', {'clasf_Proveedor_Tabla': result})
-'''
-
-
-'''
-
-    conn = pyodbc.connect('Driver={sql server};'
-                      'Server=gsvwdb17\sql2014;'
-                      'Database=Pruebas3;'
-                      'UID=gsvreportes;'
-                      'PWD=Ind2019&;'
-                      'Trusted_Connection=no;')
-    cursor = conn.cursor()
-    cursor.execute("select * from zClasifProveedores")
-    result = cursor.fetchall()
-
-    #Logica para procesar el formulario cuando se envia 
-    if request.method == 'POST' and form.is_valid():
-        #Obtiene elementos seleccionados de los campos del formulario
-        elementos_izquierda = form.cleaned_data['elementos_izquierda']
-        elementos_derecha = form.cleaned_data['elementos_derecha']
-        '''
-
-'''
-def tabla(request):
-    conn = pyodbc.connect('Driver={sql server};'
-                      'Server=gsvwdb17\sql2014;'
-                      'Database=Pruebas3;'
-                      'UID=gsvreportes;'
-                      'PWD=Ind2019&;'
-                      'Trusted_Connection=no;')
-    cursor = conn.cursor()
-    cursor.execute("select * from zClasifProveedores")
-    result = cursor.fetchall()
-    return render(request, 'Tabla.html', {'clasf_Proveedor_Tabla': result})
-#ORM https://stackoverflow.com/questions/372885/how-do-i-connect-to-a-mysql-database-in-python/622308#622308
-
-'''
+# ss
 def prueba (request):
     
      
@@ -128,7 +45,9 @@ def get_proveedores_list(request):
     
     cursor = conn.cursor()
     cursor.execute("SELECT * FROM cxp_proveedor")
-    
+    #cursor.execute("Select p.no_clabe, p.id_proveedor, p.num_proveedor, p.nombre_proveedor, c.* from cxp_proveedor p left join zClasifProv c on p.no_clabe = c.id_clasif_prov ")
+
+
     for row in cursor.fetchall():
         proveedores_list.append({
             "id_proveedor": row[0],
@@ -165,10 +84,85 @@ def get_proveedores_list(request):
             "critico": row[31],
             "contacto": row[32],
             "prod_serv": row[33],
-            "portal_multiple": row[34]
+            "portal_multiple": row[34]#,
+            #"id_clasif": row[35],
+            #"descripcion_clasif": row[36]
         })
+    #paginator = Paginator(proveedores_list, 3)
+    #pagina = request.Get.get("page") or 1
+    #posts = paginator.get_page(pagina)
+    #pagina_actual = int(pagina)
+    #paginas = range(1, posts.paginator.num_pages + 1)
     cursor.close()
     conn.close()
     return render (request, 'proveedores_list.html', {'proveedores_list':proveedores_list})
+
+def addProv(request):
+    if request.method == 'GET':
+        return render(request, 'addProv.html')
+    if request.method == 'POST':
+        form = ProvForm (request.POST)
+        if form.is_valid():
+            id_proveedor = form.cleaned_data.get("id_proveedor")
+            num_proveedor = form.cleaned_data.get("num_proveedor")
+            nombre_proveedor = form.cleaned_data.get("nombre_proveedor")
+            razon_social = form.cleaned_data.get("razon_social")
+            modulo_origen = form.cleaned_data.get("modulo_origen")
+        conn = pyodbc.connect('Driver={sql server};'
+                          'Server=gsvwdb17\sql2014;'
+                          'Database=Pruebas3;'
+                          'UID=gsvreportes;'
+                          'PWD=Ind2019&;'
+                          'Trusted_Connection=no;')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO dbo.cxp_proveedor(id_proveedor, num_proveedor, nombre_proveedor, razon_social, modulo_origen) VALUES (?, ?, ?, ?, ?)", 
+                       id_proveedor, num_proveedor, nombre_proveedor, razon_social, modulo_origen)
+        conn.commit()
+        conn.close()
+
+
+        return render(request, 'addProv.html', {'proveedores':{}})
+    
+def updateprov(request, id_proveedor):
+    pv = []
+    conn = pyodbc.connect('Driver={sql server};'
+                          'Server=gsvwdb17\sql2014;'
+                          'Database=Pruebas3;'
+                          'UID=gsvreportes;'
+                          'PWD=Ind2019&;'
+                          'Trusted_Connection=no;')
+    cursor = conn.cursor()
+    if request.method == 'GET':
+        cursor.execute("SELECT * FROM dbo.cxp_proveedor WHERE id_proveedor = ?", id_proveedor)
+        for row in cursor.fetchall():
+            pv.append({"id_proveedor": row[0], "num_proveedor": row[1], "nombre_proveedor": row[2], "razon_social": row[3], "modulo_origen": row[4]})
+        conn.close()
+        return render (request, "addProv.html", {'proveedor':pv[0]})
+    if request.method == 'POST':
+        form = ProvForm(request)
+        if form.is_valid():
+            num_proveedor = str(form.cleaned_data.get("num_proveedor"))
+            nombre_proveedor = str(form.cleaned_data.get("nombre_proveedor"))
+            razon_social = str(form.cleaned_data.get("razon_social"))
+            modulo_origen = str(form.cleaned_data.get("modulo_origen"))
+            cursor.execute("UPDATE dbo.cxp_proveedor SET num_proveedor = ?, nombre_proveedor = ?, razon_social= ?, modulo_origen = ?", 
+                           num_proveedor, nombre_proveedor, razon_social, modulo_origen)
+            conn.commit()
+        conn.close()
+        return redirect('get_proveedores_list')
+    
+def deleteprov(request, id_proveedor ):
+    conn = pyodbc.connect('Driver={sql server};'
+                          'Server=gsvwdb17\sql2014;'
+                          'Database=Pruebas3;'
+                          'UID=gsvreportes;'
+                          'PWD=Ind2019&;'
+                          'Trusted_Connection=no;')
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM dbo.cxp_proveedores where id_proveedor = ?", id_proveedor)
+    conn.commit()
+    conn.close()
+    return redirect('get_proveedores_list')
+    
 
 
